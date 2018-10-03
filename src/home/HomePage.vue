5127fbd0d2e5fb47458ac2581fa5a42d7ca2ab1f
ODarian <darian.onaciu@endava.com>
2018-10-03 18:26:26 +0300

    commit

---
+++ src/home/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <div>
+            <img class="robot" src="../assets/robot-home.png" aria-hidden="true"/>
+        </div>
+        <div class="get-started">
+            <a href="">Get started</a> building your first robot!
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomePage",
+        data() {
+            return {
+                msg: "Welcome to Your Vue.js App"
+            };
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
